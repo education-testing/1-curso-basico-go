3f3e020f7cd5f365bd40c3df34d5031866e0c759
Luis Eduardo Bolivar Cardenas <theluisbolivar@Luiss-MacBook-Pro.local>
2022-03-12 22:06:41 -0500

    Studyng variables

--- main.go
+++ main.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello World!")
+	// Declaración de constantes.
+	const pi float32 = 3.14
+	const pi2 = 3.1415
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	//Declaración de variables de forma 1:
+	base := 12
+	fmt.Println(base)
+
+	//Declaración de variables de forma 2:
+	var altura int = 32
+	fmt.Println(altura)
+
+	//Declaración de variables de forma 3:
+	var anchura int
+	fmt.Println(anchura)
+
+	var a int
+	var b float32
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
 }

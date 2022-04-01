d04f9ff11c03a9620532de93576d6868b04f0ce9
Luis Eduardo Bolivar Cardenas <theluisbolivar@192.168.1.11>
2022-03-31 19:52:50 -0600

    finished test keywords

--- main.go
+++ main.go
@@ -13,9 +13,35 @@ func findDuplicate(nums []int) int {
 		fmt.Println(index)
 	}
 
+	testContinue(nums)
+	testBreak(nums)
+
 	return 1
 }
 
 func printDefer(nums []int) {
 	fmt.Println(nums)
+}
+
+func testContinue(nums []int) {
+	fmt.Println("Starting continue test")
+	for i := 0; i < len(nums); i++ {
+
+		if nums[i] == 2 {
+			continue
+		}
+
+		fmt.Println(nums[i])
+	}
+}
+
+func testBreak(nums []int) {
+	fmt.Println("Starting break test")
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 2 {
+			break
+		}
+
+		fmt.Println(nums[i])
+	}
 }

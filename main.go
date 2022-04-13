f9d279aaf21d20bddc23f4c6c17890d0161dca63
Luis Eduardo Bolivar Cardenas <theluisbolivar@192.168.1.7>
2022-04-12 20:09:59 -0500

    Slides and arrays

--- main.go
+++ main.go
@@ -3,45 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 3, 4, 2, 2}
-	fmt.Println(findDuplicate(nums))
-}
+	//Los arrays son immutables.
+	var array [7]int8
+	array[0] = 32
+	array[6] = 1
 
-func findDuplicate(nums []int) int {
-	defer printDefer(nums)
-	for index := range nums {
-		fmt.Println(index)
-	}
+	tamano := len(array)
+	capacidad := cap(array)
 
-	testContinue(nums)
-	testBreak(nums)
+	fmt.Println("Tamaño del array: ", tamano)
+	fmt.Println("Capacidad del array: ", capacidad)
+	fmt.Println(array)
 
-	return 1
-}
+	slice := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(slice)
+	fmt.Println("len(): ", len(slice))
+	fmt.Println("cap(): ", cap(slice))
 
-func printDefer(nums []int) {
-	fmt.Println(nums)
-}
+	/*Jugando con los slices y los arrays*/
 
-func testContinue(nums []int) {
-	fmt.Println("Starting continue test")
-	for i := 0; i < len(nums); i++ {
+	//Imprimiendo la primer posición
+	fmt.Println(array[0])
+	fmt.Println(slice[0])
 
-		if nums[i] == 2 {
-			continue
-		}
+	//Imprimiendo hasta la posición 3
+	fmt.Println(array[:3])
+	fmt.Println(slice[:3])
 
-		fmt.Println(nums[i])
-	}
-}
+	//Imprimiendo los elementos del indice 2 la indice 4
+	fmt.Println(array[2:4])
+	fmt.Println(slice[2:4])
 
-func testBreak(nums []int) {
-	fmt.Println("Starting break test")
-	for i := 0; i < len(nums); i++ {
-		if nums[i] == 2 {
-			break
-		}
+	//Imprimiendo del indice 4 en adelante.
+	fmt.Println(array[4:])
+	fmt.Println(slice[4:])
 
-		fmt.Println(nums[i])
-	}
+	//Agregando un nuevo valor :D
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	//Creamos un nuevo slice para agregarlo en el original.
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
+
+	slice = append(slice, 8)
+	slice = append(slice, 9)
+	slice = append(slice, 10)
 }

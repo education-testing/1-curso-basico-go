3846fd88f1f2fc351f03614ac808e096cc0f8ee4
Luis Eduardo Bolivar Cardenas <theluisbolivar@192.168.1.7>
2022-04-12 20:36:24 -0500

    add palindrome function

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	slice := []string{"Luis", "pedro", "Carlos"}
@@ -8,4 +11,16 @@ func main() {
 	for i, value := range slice {
 		fmt.Println("Indice: ", i, "Value: ", value)
 	}
+
+	word := "amor a roma"
+	fmt.Println(isPalindrome(strings.ToLower(word)))
+}
+
+func isPalindrome(word string) bool {
+	var result string
+	for i := len(word) - 1; i >= 0; i-- {
+		result += string(word[i])
+	}
+
+	return result == word
 }
